67fd1036567705feae75cd637d23a7c584e23d19
Justin Vongdara <HOME@Justins-MacBook-Pro.local>
2022-02-02 17:44:25 -0500

    complete lab

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log('test')
+const input = document.querySelector('#text-field')
+const btn = document.querySelector('#submit-button')
+const rst = document.querySelector('#reset-button')
+const unList = document.querySelector('#todo-list')
+
+btn.addEventListener('click', createList)
+rst.addEventListener('click', resetList)
+unList.addEventListener('click', removeTask)
+
+function createList(){
+  let list = document.createElement('li')
+  let text = input.value 
+  list.textContent = text
+  if (input.value !== ''){
+    unList.appendChild(list)
+    input.value = ''
+    input.focus();
+  }
+}
+
+function resetList(){
+  unList.innerHTML = ''
+  input.value = ''
+}
+
+function removeTask(evt){
+  evt.target.remove()
+}
+
